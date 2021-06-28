adfbd571c6191e64b1b50d5c1efd0f00d560b217
Alexander Gill <alex@skalene.com>
2021-06-28 11:34:28 +0100

    add example method

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
   data() { return {
     isSerialSupported: 'serial' in navigator,
     availablePorts: []
-  } }
+  } },
+  methods: {
+    inform() { alert('hello'); }
+  }
 });
 const vm = app.mount('#app');
 console.log(vm.isSerialSupported);
