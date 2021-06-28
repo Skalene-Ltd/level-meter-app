502dfa5eeedc26e83387525a9b872d9613ed9b5a
Alexander Gill <alex@skalene.com>
2021-06-28 15:55:48 +0100

    create serial selector

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const app = Vue.createApp({
   data() { return {
     isSerialSupported: 'serial' in navigator,
-    availablePorts: [],
-    selectedPort: null
+    serialPort: null
   } },
   methods: {
     async connect() {
       const Serial = navigator.serial;
-      const port = await Serial.requestPort();
-      this.availablePorts.push(port);
-      this.selectedPort = port;
+      this.serialPort = await Serial.requestPort();
     }
   }
 });
 
-app.component('serial-port-item', {
+app.component('serial-port-details', {
   props: ['port'],
-  template: `<li>a port</li>`
+  data() { return {
+    usbVendorId: undefined,
+    usbProductId: undefined
+  } },
+  template: `<div class="app-serial-port-item">
+    <h3>Vendor</h3>
+    <p>{{ usbVendorId || 'unknown vendor' }}</p>
+    <h3>Product ID</h3>
+    <p>{{ usbProductId || 'unknown product' }}</p>
+  </div>`,
+  methods: {
+    async getInfo() {
+      const { usbVendorId, usbProductId } = await this.port.getInfo();
+      this.usbVendorId = usbVendorId;
+      this.usbProductId = usbProductId;
+    }
+  },
+  created() { this.getInfo(); },
+  beforeUpdate() { this.getInfo(); }
 });
 
 const vm = app.mount('#app');
