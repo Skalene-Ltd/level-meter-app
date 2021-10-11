e1daa066cb08578063c7b355b86bfe93790e01bb
Alexander Gill <alex@skalene.com>
2021-10-11 12:40:33 +0100

    add test for string inputs

--- tests/netlify_functions/index.test.js
+++ tests/netlify_functions/index.test.js
@@ -3,9 +3,20 @@ const lib = require('../../netlify/functions/lib');
 test('creates correct points from array of numbers', () => {
   const input = [5, 7, 2, 3];
   expect(lib.generatePointsAt10msIntervals(input)).toEqual([
-    [0,  5],
+    [0, 5],
     [10, 7],
     [20, 2],
     [30, 3]
+  ]);
+});
+
+test('creates correct points from array of base-ten strings', () => {
+  const input = ['8', '1', '10', '03', '17'];
+  expect(lib.generatePointsAt10msIntervals(input)).toEqual([
+    [0, 8],
+    [10, 1],
+    [20, 10],
+    [30, 3],
+    [40, 17]
   ]);
 });
